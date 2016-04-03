d46972cea43f263acb2a5d3480617d46dbc9dab1
Chris Sawczuk <gummybassist@gmail.com>
2016-04-03 21:02:35 +0000

    Add collaboration tests for udev_monitor api

--- test/monitor_test.cpp
+++ test/monitor_test.cpp
@@ -6,11 +6,44 @@
 
 extern "C" {
 	int udev_monitor_enable_receiving_return_value = 0;
+	int udev_monitor_get_fd_return_value = 0;
+	const char *udev_monitor_new_from_netlink_captured_name = nullptr;
+
+	extern struct udev_monitor* __real_udev_monitor_new_from_netlink(struct udev*, const char*);
 
 	int __wrap_udev_monitor_enable_receiving(struct udev_monitor*)
 	{
 		return udev_monitor_enable_receiving_return_value;
 	}
+
+	int __wrap_udev_monitor_get_fd(struct udev_monitor*)
+	{
+		return udev_monitor_get_fd_return_value;
+	}
+
+	struct udev_monitor *__wrap_udev_monitor_new_from_netlink(struct udev* udev, const char *captured_name)
+	{
+		udev_monitor_new_from_netlink_captured_name = captured_name;
+		return __real_udev_monitor_new_from_netlink(udev, captured_name);
+	}
+}
+
+TEST(UdevMonitor, monitorWillBeCreatedForUdevByDefault) {
+
+	udev_monitor_enable_receiving_return_value = -1;
+
+	Udev::Udev udev;
+	auto monitor = udev.monitor_new_from_netlink();
+	ASSERT_EQ(udev_monitor_new_from_netlink_captured_name, "udev");
+}
+
+TEST(UdevMonitor, monitorCanBeCreatedForKernelEvents) {
+
+	udev_monitor_enable_receiving_return_value = -1;
+
+	Udev::Udev udev;
+	auto monitor = udev.monitor_new_from_netlink("kernel");
+	ASSERT_EQ(udev_monitor_new_from_netlink_captured_name, "kernel");
 }
 
 TEST(UdevMonitor, willThrowWhenItIsNotPossibleToEnableReceiving) {
@@ -29,4 +62,13 @@ TEST(UdevMonitor, willNotThrowWhenItIsPossibleToEnableReceiving) {
 	Udev::Udev udev;
 	auto monitor = udev.monitor_new_from_netlink();
 	ASSERT_NO_THROW(monitor.enable_receiving());
+}
+
+TEST(UdevMonitor, willReturnTheCorrectSocketFd) {
+
+	udev_monitor_get_fd_return_value = 0xf;
+
+	Udev::Udev udev;
+	auto monitor = udev.monitor_new_from_netlink();
+	ASSERT_EQ(monitor.get_fd(), udev_monitor_get_fd_return_value);
 }
